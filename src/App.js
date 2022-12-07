93944d38d1eccc6239e3a17af7f54ad9241789f3
Abhay Pareek <abhaypareek170@gmail.com>
2022-12-07 11:39:38 +0530

    Custom Hook

--- src/App.js
+++ src/App.js
@@ -5,58 +5,56 @@ import { mailActions } from "./store/MailSlice";
 import Login from "./Components/Auth/Login";
 import SignUp from "./Components/Auth/SignUp";
 import HomePage from "./Pages/HomePage";
-import MailDetails from "./Pages/MailDetails"
+import MailDetails from "./Pages/MailDetails";
 import axios from "axios";
 import { useEffect } from "react";
 import SentPage from "./Pages/SentPage";
 
-
 function App() {
   const dispatch = useDispatch();
   const userId = localStorage.getItem("email").replace(/[@,.]/g, "");
-   useEffect(()=>{
-   const interval =  setInterval(()=>{
+  useEffect(() => {
+    const interval = setInterval(() => {
       axios
-    .get(
-      `https://mail-box-client-860d7-default-rtdb.firebaseio.com/mails/${userId}/inbox.json`
-    )
-    .then((res) => {
-      let datas = res.data;
-      console.log(res.data)
-      let mailArray = [];
-      for (let id in datas) {
-        let mail = datas[id];
-        mail.id = id;
-        mailArray.push(mail);
-      }
-      dispatch(mailActions.addMail(mailArray));
-      console.log("calling backend")
-    })
-    return () => clearInterval(interval);
-    },2000)
-    
-   },[dispatch,userId]) 
-   useEffect(()=>{
-    const interval= setInterval(()=>{
+        .get(
+          `https://mail-box-client-860d7-default-rtdb.firebaseio.com/mails/${userId}/inbox.json`
+        )
+        .then((res) => {
+          let datas = res.data;
+          console.log(res.data);
+          let mailArray = [];
+          for (let id in datas) {
+            let mail = datas[id];
+            mail.id = id;
+            mailArray.push(mail);
+          }
+          dispatch(mailActions.addMail(mailArray));
+          console.log("calling backend");
+        });
+      return () => clearInterval(interval);
+    }, 2000);
+  }, [dispatch, userId]);
+  useEffect(() => {
+    const interval = setInterval(() => {
       axios
-    .get(
-      `https://mail-box-client-860d7-default-rtdb.firebaseio.com/mails/${userId}/sent.json`
-    )
-    .then((res) => {
-      let datas = res.data;
-      console.log(res.data)
-      let mailArray = [];
-      for (let id in datas) {
-        let mail = datas[id];
-        mail.id = id;
-        mailArray.push(mail);
-      }
-      dispatch(mailActions.addSentMail(mailArray));
-      console.log("calling backend2")
-    })
-    },2000)
+        .get(
+          `https://mail-box-client-860d7-default-rtdb.firebaseio.com/mails/${userId}/sent.json`
+        )
+        .then((res) => {
+          let datas = res.data;
+          console.log(res.data);
+          let mailArray = [];
+          for (let id in datas) {
+            let mail = datas[id];
+            mail.id = id;
+            mailArray.push(mail);
+          }
+          dispatch(mailActions.addSentMail(mailArray));
+          console.log("calling backend2");
+        });
+    }, 2000);
     return () => clearInterval(interval);
-   },[dispatch,userId]) 
+  }, [dispatch, userId]);
   return (
     <>
       <BrowserRouter>
@@ -64,8 +62,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signUp" element={<SignUp />} />
-          <Route path="/message" element={<MailDetails/>}/>
-          <Route path="/sent" element={<SentPage/>}></Route>
+          <Route path="/message" element={<MailDetails />} />
+          <Route path="/sent" element={<SentPage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/Hook/useFetch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (dataUrl) => {
+  const [data, setData] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const fetchData = async (url) => {
+        try {
+          const response = await axios.get(url);
+          setData(response.data);
+          setFetchError(null);
+        } catch (err) {
+          setFetchError(err.message);
+          setData([]);
+        }
+      };
+      fetchData(dataUrl);
+    }, [dataUrl]);
+    return () => clearInterval(interval);
+  }, 2000);
+  return { data, fetchError };
+};
+
+export default useFetch;
